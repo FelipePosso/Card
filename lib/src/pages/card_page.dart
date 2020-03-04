f1a0b22f75659a335504f0b484bfce4951c9d45a
FelipePosso <60272219+FelipePosso@users.noreply.github.com>
2020-03-04 12:52:59 -0500

    Versión Final

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -5,140 +5,256 @@ class CardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cards'),
+        leading: Icon(
+          Icons.chevron_left,
+          color: Colors.black,
+        ),
+        title: Text(
+          'Credit Cards',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
       ),
       body: ListView(
         padding: EdgeInsets.all(10),
-        children: <Widget>[_cardTipo2(), _cardTipo3()],
+        children: <Widget>[
+          _cardTipo1(),
+          SizedBox(
+            height: 20,
+          ),
+          _cardTipo2(),
+          _cardTipo3()
+        ],
       ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Stack(
+      children: <Widget>[
+        Padding(
+            padding: const EdgeInsets.all(3),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              elevation: 6,
+              child: Stack(
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image(
+                      image: NetworkImage(
+                        "https://crehana-public-catalog.imgix.net/images/courses/promo-images/b4363186/8495d5fa.jpg?w=630&h=383&q=50&fit=crop",
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Column(children: <Widget>[
+                      Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(children: <Widget>[
+                            Padding(
+                                padding: const EdgeInsets.only(top: 0),
+                                child: Column(
+                                  children: <Widget>[
+                                    ListTile(
+                                      title: Text(
+                                        'Finaci',
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w900,
+                                            color: Colors.white),
+                                      ),
+                                    ),
+                                    ListTile(
+                                      subtitle: Text(
+                                        '**** **** **** 3728',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w600,
+                                            color: Colors.white),
+                                      ),
+                                    ),
+                                    Row(
+                                      children: <Widget>[
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 12),
+                                          child: Text(
+                                            'Card Holder name',
+                                            style: TextStyle(
+                                                fontSize: 10,
+                                                color: Colors.white),
+                                          ),
+                                        ),
+                                        Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 50),
+                                            child: Text(
+                                              'Expiry date',
+                                              style: TextStyle(
+                                                  fontSize: 10,
+                                                  color: Colors.white),
+                                            )),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: <Widget>[
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 12),
+                                          child: Text(
+                                            'Austin Hammond',
+                                            style: TextStyle(
+                                                fontSize: 15,
+                                                color: Colors.white),
+                                          ),
+                                        ),
+                                        Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 15),
+                                            child: Text(
+                                              '02/30',
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  color: Colors.white),
+                                            )),
+                                        Stack(
+                                          children: <Widget>[
+                                            Padding(
+                                              padding: const EdgeInsets.symmetric(
+                                                  horizontal: 22, ),
+                                              child: Icon(Icons.brightness_1,
+                                                  color: Colors.white),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.symmetric(
+                                                  horizontal: 35, ),
+                                              child: Icon(Icons.trip_origin,
+                                                  color: Colors.white),
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                )),
+                          ])),
+                    ]),
+                  ),
+                ],
+              ),
+            )),
+      ],
     );
   }
 
   Widget _cardTipo2() {
     return Padding(
-      padding: const EdgeInsets.only(top: 5.0, left: 5.0, bottom: 20.0, right: 5.0),
+      padding:
+          const EdgeInsets.only(top: 5.0, left: 5.0, bottom: 20.0, right: 5.0),
       child: Card(
-        elevation: 6,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15))),
+          elevation: 6,
           child: Column(children: <Widget>[
-        Row(
-          children: <Widget>[
-            
             Padding(
-              padding: const EdgeInsets.only(top: 12.0, left: 12.0, bottom: 12.0, right: 50.0),
-              child: Text('Your banck accounts', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600, ),),
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 12.0, left: 12.0, bottom: 12.0, right: 40.0),
+                    child: Text(
+                      'Your banck accounts',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50))),
+                    elevation: 5,
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 12.0, left: 12.0, bottom: 12.0, right: 10.0),
+                      child: Text(
+                        'See All',
+                        style: TextStyle(color: Colors.blueGrey[100]),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-            RaisedButton(
-              onPressed: () {}, shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(50))), elevation: 5, color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 12.0, left: 12.0, bottom: 12.0, right: 10.0),
+            Container(
+              margin: EdgeInsets.only(bottom: 32),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    title: Text(
+                      'Austin Hammond',
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                    leading: FloatingActionButton(
+                      child: Icon(Icons.account_balance),
+                      onPressed: () {},
+                      elevation: 0,
+                    ),
+                    subtitle: Text(
+                      'IBAN: US34-INGB 0001 9999 0012 1569 \n BIC / SWIFT - INGBUSBU',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ])),
+    );
+  }
+
+  Widget _cardTipo3() {
+    return Container(
+      margin: EdgeInsets.all(5),
+      child: Card(
+          color: Colors.blue[400],
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20))),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: ListTile(
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 12.0),
                 child: Text(
-                  'See All',
-                  style: TextStyle(color: Colors.blueGrey[100]),
+                  'Invite Friends. Get reward',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w900,
+                  ),
                 ),
               ),
-              
-            )
-          ],
-
-        ),
-        ListTile(
-          title: Text('gsf'),
-          leading: FloatingActionButton(onPressed: (){}, elevation: 0, child: Icon(Icons.hotel),),
-          subtitle: Text('ahfdhd'),
-        )
-      ])),
-    );
-  }
-
-  Widget _cardTipo3() {
-    return Card(
-        color: Colors.lightBlue,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(20))),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: ListTile(
-            title: Padding(
-              padding: const EdgeInsets.only(bottom: 12.0),
-              child: Text(
-                'Invite Friends. Get reward',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w900,
+              subtitle: Text(
+                  'Send your friends invitations and win a free Premium account',
+                  style: TextStyle(color: Colors.white)),
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 0, right: 0),
+                child: FloatingActionButton(
+                  onPressed: () {},
+                  elevation: 0,
+                  child: Icon(
+                    Icons.account_balance,
+                    color: Colors.lightBlue,
+                  ),
+                  backgroundColor: Colors.white,
                 ),
               ),
             ),
-            subtitle: Text(
-                'Send your friends invitations and win a free Premium account',
-                style: TextStyle(color: Colors.white)),
-            leading: FloatingActionButton(
-              onPressed: null,
-              child: Icon(
-                Icons.home,
-                color: Colors.lightBlue,
-              ),
-              backgroundColor: Colors.white,
-            ),
-          ),
-        ));
+          )),
+    ); 
   }
-  /*
-            title: Text('Finaci')
-            subtitle: Text('**** **** **** 3728'),
-            leading: Text('Card Holder name') ,
-            trailing: Text('Expiry date'),    
-            trailing: Icon(Icons.brightness_1),
-          */
-  /*Widget _cardTipo2() {
-    return Card(
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              FlatButton(
-                onPressed: null, child: Text('See all')
-              ),
-            ]
-          ),  
-          ListTile(
-            title: Text('Your banck accounts'),
-          ),
-          ListTile(
-            leading: Icon(Icons.account_balance, color: Colors.lightBlue, size: (40),)
-            ),
-          ListTile(
-            title: Text('Austin Hammond'),
-          ),
-          ListTile( 
-            subtitle: Text('IBAN: US34-INGB 0001 9999 0012 1569'),
-          ),
-          ListTile( 
-            subtitle: Text('BIC / SWIFT - INGBUSBU'),
-          ),
-
-        ],
-      ),
-    );
-  }
-          
-  Widget _cardTipo3() {
-    return Card(
-      child: Column(
-        
-        children: <Widget>[
-        ListTile(
-            leading: Icon(Icons.account_balance, color: Colors.lightBlue, size: (40),)
-        ),
-        ListTile(
-            title: Text('Invite Friends. Get reward'),
-          ),
-        ListTile( 
-            subtitle: Text('Send your friends invitations and win a free Premium account'),
-          ),
-        ],
-      ),
-    );
-  }*/
 }
